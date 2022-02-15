e34b73d82758d09a9cd77f9c4fc04ef4196b3762
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-15 23:15:24 +0530

    added uc2 problem

---
+++ Snake_ladder_UC2.java
@@ -0,0 +1,14 @@
+
+public class Snake_ladder_UC2 {
+
+	
+	
+	public static void main(String[] args) {
+		
+		int i = (int) (Math.floor(Math.random() * 10) % 6)+1;
+		{
+		 System.out.println("player rolls die and gets value =" +i);
+		}
+	}
+
+}
