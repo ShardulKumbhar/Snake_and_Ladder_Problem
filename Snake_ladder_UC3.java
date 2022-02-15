5aa661b24068fa41b71b21ddd881e6494eabb867
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-15 23:18:55 +0530

    added uc4 snake and ladder problem

---
+++ Snake_ladder_UC3.java
@@ -0,0 +1,30 @@
+
+public class Snake_ladder_UC3 {
+
+	public static void main(String[] args) {
+		int i=0;
+		int noPlay=0;
+		int ladder=1;
+		int snake=2;
+			while(i<=20)
+			{
+				int roll = (int) Math.floor((Math.random() * 10) % 3);
+				int value = (int) Math.floor((Math.random() * 10) % 6)+1;
+				System.out.println("Dice Roll =" + value);
+				if(roll == noPlay) 
+				{
+					System.out.println("No Play" + i);
+				}
+				else if (roll == ladder) 
+				{	
+					i=i+value;
+					System.out.println("Ladder" + i);
+				}
+	    	   else 
+	    	   {
+				i=i-value;
+				System.out.println("snake" + i);
+			   }
+	}
+  }
+}		
